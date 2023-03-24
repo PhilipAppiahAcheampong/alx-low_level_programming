6e10c1e33b3245013b750501f467ca068cd86142
philipappiahacheampong <philipappiahacheampong@gmial.com>
2023-03-24 03:11:33 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -10,19 +8,18 @@ int _islower(int c);
 
 int _isalpha(int c);
 
-int  print_sign(int n);
+int print_sign(int n);
 
 int _abs(int);
 
 int print_last_digit(int);
 
-void jack_baver(void);
+void jack_bauer(void);
 
 void times_table(void);
 
-int add(int, int);
+void add(int, int);
 
 void print_to_98(int n);
 
 void print_times_table(int n);
-#endif
